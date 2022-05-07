a10efff9c852cab2d26ebb32ecc69cfa4cedd386
cyanidle <lyosha.doronin@gmail.com>
2022-05-07 21:02:28 +0200

    kek

--- TestLib/dLinearAlg.cpp
+++ TestLib/dLinearAlg.cpp
@@ -1,6 +1,11 @@
 #include "dLinearAlg.h"
 
-dTransformer2d::dTransformer2d(float rot) 
+dTransformer2d::dTransformer2d(float rot = 0, float scale = 0, float transform = 0) 
 {
 	m_size = 9;
+}
+
+
+dPoint2f::dPoint2f(float x, float y, float theta = 0):m_theta(theta) {
+
 }
--- TestLib/dLinearAlg.h
+++ TestLib/dLinearAlg.h
@@ -1,55 +1,59 @@
 #pragma once
 #include <float.h>
-
+#include <vector>
+#include <array>
 class dMatrix {
 public:
+	dMatrix(float = 0, float = 0, float = 0); // just rotation
 	virtual dMatrix& operator=(dMatrix&) noexcept;
-	virtual dMatrix& operator*(dMatrix&);
-	virtual dMatrix& operator+(dMatrix&);
-	virtual dMatrix& operator-(dMatrix&);
-	virtual dMatrix& operator/(dMatrix&);
+	virtual dMatrix operator*(dMatrix) const;
+	virtual dMatrix operator+(dMatrix) const;
+	virtual dMatrix operator-(dMatrix) const;
+	virtual dMatrix operator/(dMatrix) const;
+	virtual dVect& apply(dVect&);
+	virtual dMatrix& changeRot(float);
+	virtual dMatrix& changeScale(float);
+	virtual dMatrix& changeMove(float);
 protected:	
 	unsigned int m_size;
 };
 class dVect {
 public:
+	dVect(std::vector<float>);
+	dVect(float = 0, float = 0, float = 0);
 	virtual dVect& operator=(dVect&) noexcept;
-	virtual dVect& operator*(dVect&);
-	virtual dVect& operator+(dVect&);
-	virtual dVect& operator-(dVect&);
-	virtual dVect& operator/(dVect&);
+	virtual dVect operator*(dVect&) const;
+	virtual dVect operator+(dVect&) const;
+	virtual dVect operator-(dVect&) const;
+	virtual dVect operator/(dVect&) const;
+	//
+	virtual dVect& applyTrans(dMatrix);
+	virtual dVect operator*(dMatrix&) const;
+	virtual dVect& applyRotate(float);
+	virtual dVect& applyScale(float);
+	virtual dVect& applyMove(float);
 protected:
 	unsigned int m_size;
 };
 class dPoint2f : private dVect
 {
 public:
-	dPoint2f();
-	dPoint2f(float);
-	dPoint2f(float, float);
-	dPoint2f& applyTrans(dTransformer2d);
+	dPoint2f(float = 0, float = 0, float = 0);
 	dPoint2f& operator*(dTransformer2d);
-	dPoint2f& applyRotate(float);
-	dPoint2f& applyScale(float);
-	dPoint2f& applyMove(float);
 	dPoint2f& operator=(dPoint2f&) noexcept;
 	dPoint2f& operator*(dPoint2f&);
 	dPoint2f& operator+(dPoint2f&);
 	dPoint2f& operator-(dPoint2f&);
 	dPoint2f& operator/(dPoint2f&);
 private:
-	float m_point[];
+	std::array<float, 2> m_point;
+	float m_theta;
 };
 class dTransformer2d : private dMatrix
 {
 public:
-	dTransformer2d(float, float = 0, float = 0);
-	dTransformer2d(float);
-	dPoint2f apply(dPoint2f);
-	dPoint2f& apply(dPoint2f&);
-	dTransformer2d& changeRot(float);
-	dTransformer2d& changeScale(float);
-	dTransformer2d& changeMove(float);
+	dTransformer2d(float = 0, float = 0, float = 0);
+	
 private:
-	float m_mat[];
+	std::array<float, 9> m_mat;
 };
